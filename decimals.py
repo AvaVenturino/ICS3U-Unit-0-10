0786200eb09184837933958f020727dca0ed8cc0
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2023-03-11 02:55:37 +0000

    added python

---
+++ decimals.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Created by: Ava Venturino
+Created on: Mar 2023
+This program determines if a 
+specific year is a leap year
+"""
+
+
+def main():
+
+    # input &process & output
+    answer = 0.1 + 0.2
+    print(f"0.1 + 0.2 = {answer}")
+
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
